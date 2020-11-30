0d4d796a3bc036f5f787ee53385bbf4f97776098
Bjornraaf <daysforyou1120@gmail.com>
2020-11-30 18:34:04 +0100

    Project met O.A. Movescript

---
+++ New Unity Project/Assets/Script/Health Stuff/Damage.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+    private int Health = 3;
+    [SerializeField] Text hitText;
+    
+    void Update()
+    {
+        if (transform.position.y <= -7)
+        {
+            Dead();
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Hit();
+        }
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            Hit();
+
+            if (Health == 0)
+            {
+                Destroy(gameObject);
+                Time.timeScale = 0;
+            } 
+        }
+    }
+
+    protected void Hit()
+    {
+        Health--;
+        Debug.Log(Health);
+        hitText.text = "HP:" + " " + Health.ToString();
+    }
+    void Dead()
+    {
+        Debug.Log("Test");
+        transform.position = new Vector3(-10, -1.461544f, 0);
+    }
+}
