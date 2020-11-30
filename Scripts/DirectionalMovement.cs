7d5eb6be7692b3dde96a5721d2ea90b4769ec7e5
Jason Dikken <electronicjason.dikken@gmail.com>
2020-11-30 11:48:18 +0100

    enemy behavior

---
+++ Scripts/DirectionalMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionalMovement : MonoBehaviour
+{
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private float height = 4.0f;
+    [SerializeField] private float speed = 2.0f;
+
+    public void Update()
+    {
+        float movement = Mathf.PingPong(Time.time, 1) * height ;
+        Vector3 newPosition = direction * movement;
+        transform.Translate(newPosition * Time.deltaTime * speed);
+    }
+}
---
+++ Scripts/MortarFlower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarFlower : SpawnBullet
+{
+    [SerializeField] private Transform point0;
+    [SerializeField] private Transform point1;
+    [SerializeField] private Transform point2;
+
+    protected override void Shoot()
+    {
+        base.Shoot();
+        lastestSpawnedBullet.GetComponent<CurveBullet>().SetPoints(point0.position, point1.position, point2.position);
+    }
+}
