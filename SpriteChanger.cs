3b5621488084eaf4e395d6009b897690d8c3ac01
ties-postma <28114@ma-web.nl>
2020-12-04 10:55:23 +0100

    sprite thingys
    sprite changer thing

---
+++ SpriteChanger.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : PlayerMovement
+{
+	public CharacterController2D controller;
+
+public Sprite spriteA;
+public Sprite spriteB;
+public Sprite spriteC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+            idle = false;
+		}
+
+		if (Input.GetButtonUp("Jump"))
+		{
+			jump = false;
+            idle = true;
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+            idle = false;
+		}
+		else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+            idle = true;
+		}
+
+
+
+        if (jump == true)
+		{
+		GetComponent<SpriteRenderer>().sprite = spriteA;
+		}
+
+        if (crouch == true)
+		{
+		GetComponent<SpriteRenderer>().sprite = spriteB;
+		}
+
+        if (idle == true)
+		{
+		GetComponent<SpriteRenderer>().sprite = spriteC;
+		}
+
+    }
+}
