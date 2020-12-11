91a3bce01811683d0aec36c59b04fea42b581e57
Bjornraaf <daysforyou1120@gmail.com>
2020-12-11 10:06:47 +0100

    Update Damage/Health
    f

--- New Unity Project/Assets/Script/Health Stuff/Damage.cs
+++ New Unity Project/Assets/Script/Health Stuff/Damage.cs
@@ -6,16 +6,18 @@ using UnityEngine.UI;
 
 public class Damage : MonoBehaviour
 {
-    private int Health = 3;
+    protected int Health = 3;
     [SerializeField] Text hitText;
-    
+
     void Update()
     {
         if (transform.position.y <= -7)
         {
-            Dead();
+            Hit();
+            transform.position = new Vector3(-10, -1.461544f, 0);
         }
     }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
@@ -26,24 +28,19 @@ public class Damage : MonoBehaviour
         if (collision.gameObject.tag == "Wall")
         {
             Hit();
+        }
 
-            if (Health == 0)
-            {
-                Destroy(gameObject);
-                Time.timeScale = 0;
-            } 
+        if (Health == 0)
+        {
+            Destroy(gameObject);
+            Time.timeScale = 0;
         }
     }
 
-    protected void Hit()
+    void Hit()
     {
         Health--;
         Debug.Log(Health);
         hitText.text = "HP:" + " " + Health.ToString();
-    }
-    void Dead()
-    {
-        Debug.Log("Test");
-        transform.position = new Vector3(-10, -1.461544f, 0);
     }
 }
